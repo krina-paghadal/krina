1234d0065e6a3845c76c9cdb5dfccfc7e7e089a4
krina-paghadal <152617183+krina-paghadal@users.noreply.github.com>
2024-02-02 18:16:48 +0530

    second file

---
+++ PR1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ int c;
+ float f;
+ printf("enter your celsius");
+ scanf("%d",&c);
+
+ f=(c*9/5)+32;
+ printf("%f",f);
+ getch();
+
+
+}
---
+++ SWIP2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ int a=8;
+ int b=12;
+
+ a=a+b;
+ b=a-b;
+ a=a-b;
+
+ printf("a=%d \nb=%d",a,b);
+ getch();
+
+
+}
